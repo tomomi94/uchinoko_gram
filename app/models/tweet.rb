3209105cce47faa6b6c99255aabf77b0b0634c63
tomomi94 <kurochan1004@gmail.com>
2021-04-18 21:38:22 +0900

    Define association

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,7 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  has_many :comments
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :pet_type
