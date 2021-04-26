7f97c1c653e037e3e5eecad2d170540441075d6d
tomomi94 <kurochan1004@gmail.com>
2021-04-26 22:49:17 +0900

    Add Likes Root

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "tweets#index"
+  resources :users, only: :show
   resources :tweets do
     resources :comments, only: :create
     collection do
       get 'search'
     end
   end
-  resources :users, only: :show
+
+  post 'like/:id' => 'likes#create', as: 'create_like'
+  delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
+
+  
 end
