04b487ab3481283f3975e48e7dee18b803ea834d
tomomi94 <kurochan1004@gmail.com>
2021-04-26 22:47:56 +0900

    Add Like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
-  belongs_to :user
+  belogns_to :user
   belongs_to :tweet
 
   validates_uniqueness_of :tweet_id, scope: :user_id
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :accountname, presence: true, length: { maximum: 6 }
 
   has_many :tweets
-  has_many :likes
   has_many :comments
-  has_many :likes, dependent: :destroy
+  has_many :likes
 
-  validates :accountname, presence: true, length: { maximum: 6 }
-  
-  def already_liked?(tweet)
-    self.likes.exists?(tweet_id: tweet_id)
+  def liked_by?(tweet_id)
+    likes.where(tweet_id: tweet_id).exists?
   end
+  
 end
